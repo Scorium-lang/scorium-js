d408dea185a5182ad11a61e4e00bee8e33122cd8
Fi3w0 <alex.lazarevych@icloud.com>
2026-08-30 04:42:24 +0200

    Add public entry point

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { parse, ParseError } from "./parser.ts";
+export { evaluate, EvalError } from "./eval.ts";
+export type { Document, Item, LeafDecl, NodeDecl, Expr, HeaderValue } from "./ast.ts";
+export type { Value } from "./value.ts";
+export type { Entry } from "./entry.ts";
+export { SCORIUM_LANGUAGE_VERSION } from "./version.ts";
