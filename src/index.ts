aaab2ffb2fe72bb5ebf0fa38421d0f2740e9a348
Fi3w0 <alex.lazarevych@icloud.com>
2026-08-30 05:31:46 +0200

    Export EvalOptions/IncludePolicy/IncludeStmt

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { parse, ParseError } from "./parser.ts";
 export { evaluate, EvalError } from "./eval.ts";
-export type { Document, Item, LeafDecl, NodeDecl, VarDef, Expr, HeaderValue, StrLit, BarePart, UnOp, BinOp } from "./ast.ts";
+export type { EvalOptions, IncludePolicy } from "./eval.ts";
+export type { Document, Item, LeafDecl, NodeDecl, VarDef, Expr, HeaderValue, StrLit, BarePart, UnOp, BinOp, IncludeStmt } from "./ast.ts";
 export type { Value } from "./value.ts";
 export type { Entry } from "./entry.ts";
 export { SCORIUM_LANGUAGE_VERSION } from "./version.ts";
