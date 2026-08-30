c41b04661094de594db67587d73f3b02f78d6872
Fi3w0 <alex.lazarevych@icloud.com>
2026-08-30 05:44:46 +0200

    Export format(), FormatOptions, Comment, and Trivia

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export { parse, ParseError } from "./parser.ts";
 export { evaluate, EvalError } from "./eval.ts";
 export type { EvalOptions, IncludePolicy } from "./eval.ts";
-export type { Document, Item, LeafDecl, NodeDecl, VarDef, Expr, HeaderValue, StrLit, BarePart, UnOp, BinOp, IncludeStmt } from "./ast.ts";
+export { format } from "./format.ts";
+export type { FormatOptions } from "./format.ts";
+export type { Document, Item, LeafDecl, NodeDecl, VarDef, Expr, HeaderValue, StrLit, BarePart, UnOp, BinOp, IncludeStmt, Comment, Trivia } from "./ast.ts";
 export type { Value } from "./value.ts";
 export type { Entry } from "./entry.ts";
 export { SCORIUM_LANGUAGE_VERSION } from "./version.ts";
