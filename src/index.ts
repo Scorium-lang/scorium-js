c5799d3047fdac80d7bd7f71756675e40d51917d
Fi3w0 <alex.lazarevych@icloud.com>
2026-08-30 05:54:22 +0200

    Export SandboxOptions, ScoriumError, LexError, ScriptBlock

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 export { parse, ParseError } from "./parser.ts";
 export { evaluate, EvalError } from "./eval.ts";
-export type { EvalOptions, IncludePolicy } from "./eval.ts";
+export type { EvalOptions, IncludePolicy, SandboxOptions } from "./eval.ts";
 export { format } from "./format.ts";
 export type { FormatOptions } from "./format.ts";
-export type { Document, Item, LeafDecl, NodeDecl, VarDef, Expr, HeaderValue, StrLit, BarePart, UnOp, BinOp, IncludeStmt, Comment, Trivia } from "./ast.ts";
+export { ScoriumError, LexError } from "./errors.ts";
+export type { Document, Item, LeafDecl, NodeDecl, VarDef, Expr, HeaderValue, StrLit, BarePart, UnOp, BinOp, IncludeStmt, ScriptBlock, Comment, Trivia } from "./ast.ts";
 export type { Value } from "./value.ts";
 export type { Entry } from "./entry.ts";
 export { SCORIUM_LANGUAGE_VERSION } from "./version.ts";
